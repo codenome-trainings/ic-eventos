58f269962d87a791629bb4b9323bca18cea8bf93
Thiago Cunha <grupocodenome@gmail.com>
2017-07-13 01:27:09 -0300

    Formatando a página de login

---
+++ views/home.php
@@ -0,0 +1,24 @@
+<div class="container">
+
+    <div class="formulario-login">
+        <div class="logo-login">
+            <img class="img-responsive" src="assets/images/logo.png" alt="Logotipo da IC Eventos">
+        </div>
+        <div class="text-login">
+            <p class="text-center">Preencha os campos corretamente para ter acesso ao sistema. Caso tenha algum erro, entre em contato com o nosso <a href="#">suporte.</a></p>
+        </div>
+        <form class="" action="index.html" method="post">
+            <div class="form-group">
+                <label for="matricula" class="text-muted small">Matrícula</label>
+                <input class="form-control small formulario-input" type="text" name="matricula" id="matricula" placeholder="Digite sua matrícula">
+            </div>
+            <div class="form-group">
+                <label for="matricula" class="text-muted small">Senha</label>
+                <input class="form-control small formulario-input" type="password" name="matricula" id="matricula" placeholder="Digite sua matrícula">
+            </div>
+            <div class="form-group">
+                <input class="btn btn-danger form-control" type="submit" name="matricula" id="matricula" placeholder="Digite sua matrícula" value="Acessar sistema">
+            </div>
+        </form>
+    </div>
+</div>
--- views/template.php
+++ views/template.php
@@ -4,23 +4,32 @@
         <title><?php echo TITLE_SYSTEM; ?></title>
 
         <!-- CSS Links -->
+        <link rel="stylesheet" href="<?php echo BASE_URL; ?>/assets/bootstrap3/css/bootstrap.css" />
         <link rel="stylesheet" href="<?php echo BASE_URL; ?>/assets/css/bootstrap.css" />
         <link rel="stylesheet" href="<?php echo BASE_URL; ?>/assets/css/style.css" />
         <!-- Finish CSS Links -->
 
         <!-- javascript Links -->
-        <script type="text/javascript" src="<?php echo BASE_URL; ?>/assets/js/jquery-1.7.1.min.js"></script>
+        <script type="text/javascript" src="<?php echo BASE_URL; ?>/assets/bootstrap3/js/Bootstrap.js"></script>
         <script type="text/javascript" src="<?php echo BASE_URL; ?>/assets/js/script.js"></script>
         <!-- Finish javascript Links -->
 
     </head>
     <body>
-    	<header>
-    	</header>
-        <main class="container-fluid">
-            <?php $this->loadViewInTemplate($viewName, $viewData); ?>
-        </main>
-        <footer>
-        </footer>
+        <div class="faixa-de-topo"></div>
+        <div class="container-fluid">
+            <header>
+            </header>
+            <main class="container-fluid">
+                <?php $this->loadViewInTemplate($viewName, $viewData); ?>
+            </main>
+            <footer>
+                <div class="container">
+                    <div class="line-footer">
+                        <h6 class="text-center">IC 2017 - Desenvolvido por Thiago Cunha e Felipe Medeiros - Orientação: Irineu lima</h6>
+                    </div>
+                </div>
+            </footer>
+        </div>
     </body>
 </html>
